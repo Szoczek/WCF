674e7233ef7d8a43c717758e021967f741666610
Szymon Żoczek <szymon.zoczek@etinel.pl>
2019-01-13 08:35:28 +0100

    -Commi1t

---
+++ WcfService1/CystomerService.svc.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfService1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "CystomerService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select CystomerService.svc or CystomerService.svc.cs at the Solution Explorer and start debugging.
+    public class CystomerService : ICystomerService
+    {
+        public ICollection<CustomerDTO> GetCustomers()
+        {
+            using (var context = new NorthwindEntities())
+            {
+                return context.Customers
+                    .Select(x => new CustomerDTO
+                    {
+                        CustomerID = x.CustomerID,
+                        Address = x.Address,
+                        City = x.City,
+                        CompanyName = x.CompanyName,
+                        Country = x.Country
+                    }).ToArray();
+            }
+        }
+
+        public CustomerDTO GetCustomer(string customerID)
+        {
+            using (var context = new NorthwindEntities())
+            {
+
+                return context.Customers
+                    .Select(x => new CustomerDTO
+                    {
+                        CustomerID = x.CustomerID,
+                        Address = x.Address,
+                        City = x.City,
+                        CompanyName = x.CompanyName,
+                        Country = x.Country
+                    }).FirstOrDefault(x => x.CustomerID == customerID);
+            }
+        }
+
+        public bool AddCustomer(CustomerDTO customer)
+        {
+            using (var context = new NorthwindEntities())
+            {
+                try
+                {
+                    var newCustomer = new Customer
+                    {
+                        CustomerID = customer.CustomerID,
+                        Address = customer.Address,
+                        City = customer.City,
+                        CompanyName = customer.CompanyName,
+                        Country = customer.Country
+
+                    };
+
+                    context.Customers.Add(newCustomer);
+                    context.SaveChanges();
+                }
+                catch (Exception e)
+                {
+                    return false;
+                }
+
+                return true;
+            }
+        }
+    }
+}
---
+++ WcfService1/ICystomerService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfService1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "ICystomerService" in both code and config file together.
+    [ServiceContract]
+    public interface ICystomerService
+    {
+        [OperationContract]
+        ICollection<CustomerDTO> GetCustomers();
+
+        [OperationContract]
+        CustomerDTO GetCustomer(string customerID);
+
+        [OperationContract]
+        bool AddCustomer(CustomerDTO customer);
+
+
+    }
+
+    [DataContract]
+    public class CustomerDTO
+    {
+        [DataMember]
+        public string CustomerID { get; set; }
+        [DataMember]
+        public string CompanyName { get; set; }
+        [DataMember]
+        public string Address { get; set; }
+        [DataMember]
+        public string City { get; set; }
+        [DataMember]
+        public string Country { get; set; }
+    }
+}
